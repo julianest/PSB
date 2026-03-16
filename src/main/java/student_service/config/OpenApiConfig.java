8d1b5c4a01a18c0f2fe712ac725f91178fb4c57a
julian.huerfano <julian.huerfano@sofka.com.co>
2026-03-16 12:19:18 -0500

    first commit

---
+++ src/main/java/student_service/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package student_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Scotiabank Reactive Challenge API")
+                        .version("1.0")
+                        .description("Reactive API built with Spring WebFlux"));
+    }
+}
